1a9da1d3d93b7250d0190dbf8526373da8d57e9e
Dev Sanghvi <dys27@cornell.edu>
2018-02-21 19:39:40 -0500

    Update min_grouping_children_problem.py

--- Greedy Algorithms/min_grouping_children_problem.py
+++ Greedy Algorithms/min_grouping_children_problem.py
@@ -9,7 +9,7 @@ def min_groups(x=[3.0,4.1,5.0,5.1,5.4,6.7],n=5,L=1.0):
             current_index+=1
         if current_index<n:
             no_group+=1
-    if x[current_index]-x[last_index]>1:
+    if x[current_index]-x[last_index]>L:
         no_group+=1
     return no_group
                
