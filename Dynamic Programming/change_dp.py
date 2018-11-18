ead0877833a622e74e6641b43e1fa7e9cfb2bed2
Dev Sanghvi <dys27@cornell.edu>
2018-11-18 17:34:08 -0500

    Update change_dp.py

--- Dynamic Programming/change_dp.py
+++ Dynamic Programming/change_dp.py
@@ -1,23 +1,16 @@
-# Uses python3
-import sys
+def get_change(money):
+    coin_value_list=[1,3,4]
+    min_coins_needed=[0]*(money+1)
+    last_coin_used = [0]*(money+1)
+    for i in range(money+1):
+        money_value = i
+        coin_value = 1
+        for j in [coin for coin in coin_value_list if coin <= i]:
+            if min_coins_needed[i-j]+1 < money_value:
+                money_value = min_coins_needed[i-j]+1
+                coin_value = j
+        min_coins_needed[i] = money_value
+        last_coin_used[i] = coin_value
+    return min_coins_needed[money]
 
-def get_change(m):
-    arr=[1,3,4]
-    minCoins=[0]*(m+1)
-    coinsUsed=[0]*(m+1)
-    for i in range (m+1):
-        coinCount=i
-        newCoin=1
-        for j in [ c for c in arr if c<=i]:
-            if minCoins[i-j]+1<coinCount:
-                coinCount=minCoins[i-j]+1
-                newCoin=j
-        minCoins[i]=coinCount
-        coinsUsed[i]=newCoin
-    print minCoins
-    print coinsUsed
-    return (minCoins[m])
-
-if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+print(get_change(5))
