acf1da88ec6918ef22f6f42a4897014700f62462
Dev Sanghvi <dys27@cornell.edu>
2018-12-18 15:25:34 +0530

    Update SelectionSort.py

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -1,10 +1,10 @@
-def SelectionSort(a):
-    n=len(a)
+def selection_sort(arr):
+    n=len(arr)
     for i in range(n):
         min_no=i
         for j in range(i+1,n):
-            if a[j]<a[min_no]:
+            if arr[j]<arr[min_no]:
                 min_no=j
-        a[i],a[min_no]=a[min_no],a[i]
-    return a
+        arr[i],arr[min_no]=arr[min_no],arr[i]
+    return arr
             
