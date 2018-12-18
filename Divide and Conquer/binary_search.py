eff6f8e1fac4207a109aba3fef7c6773a3f27c71
Dev Sanghvi <dys27@cornell.edu>
2018-12-18 15:33:37 +0530

    Update binary_search.py

--- Divide and Conquer/binary_search.py
+++ Divide and Conquer/binary_search.py
@@ -1,21 +1,21 @@
 # Uses python3
 import sys
-def binary_search(a,x,low,high):
+def binary_search(arr,x,low,high):
     while low<=high:
         mid=low+(high-low)//2
-        if x==a[mid]:
+        if x==arr[mid]:
             return mid
-        elif x<a[mid]:
+        elif x<arr[mid]:
             high=mid-1
-        elif x>a[mid]:
+        elif x>arr[mid]:
             low=mid+1
     return -1
 
     # write your code here
 
-def linear_search(a, x):
-    for i in range(len(a)):
-        if a[i] == x:
+def linear_search(arr, x):
+    for i in range(len(arr)):
+        if arr[i] == x:
             return i
     return -1
 
@@ -24,9 +24,9 @@ if __name__ == '__main__':
     data = list(map(int, input.split()))
     n = data[0]
     m = data[n + 1]
-    a = data[1: n + 1]
+    arr = data[1: n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(binary_search(a, x,low=0,high=len(a)-1), end=' ')
+        print(binary_search(arr, x,low=0,high=len(a)-1), end=' ')
 
                             
